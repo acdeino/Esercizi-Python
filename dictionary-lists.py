e68f855838640b36c3477b3deab63d72b7bb0613
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-04 23:41:19 +0200

    _

---
+++ dictionary-lists.py
@@ -0,0 +1,11 @@
+lst = [2, 3, 4]  # this is a list
+dct = {"Name": "John", "Surname": "Smith", "Age": 27}  # this is a "dictionary"
+
+dct["Name"]  # this is how you extract the variable "Name" from the dictionary "dct"
+dct["Age"]  # this is how to extract the variable "Age" from the "dct"
+
+lst  # prints (in python cmd line) the list
+dct  # prints (in python cmd line) the dictionary
+
+# another way to write dictionaries is:
+dct2 = {"Name": ("Peter"), "Surname": ("Griffin"), "Age": (42)}

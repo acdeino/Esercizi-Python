6beeb052b615b018ccc137728869162ca8f2321e
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-09 15:35:46 +0200

    _

---
+++ basicfunctions.py
@@ -0,0 +1,14 @@
+def minutes_to_hours(minutes):
+    hours = minutes / 60.0
+    return hours
+
+
+def seconds_to_hours(seconds):
+    hours = seconds / 3600
+    return hours
+
+
+print(minutes_to_hours(70) + 10)
+print(seconds_to_hours(3895) + 20)
+
+print(seconds_to_hours(7380))
